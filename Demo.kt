1254c74eb4f5cf9a1846049bd7e678a9e19da66b
Gascognya <70791993+Gascognya@users.noreply.github.com>
2021-11-13 00:05:09 +0800

    Update Demo.kt

--- Demo.kt
+++ Demo.kt
@@ -7,12 +7,12 @@ fun parse(): KResult<Int, ParseError> = err(ParseError.Invalid)
 
 fun handle(): KResult<String, ParseError> =
     when (val res = parse()) {
-        is Ok -> res.map(Int::toString)
-        is Err -> err(res)
+        is Ok -> res.map(Int::toString) //KResult<Int,ParseError> -(map)-> <String,ParseError>
+        is Err -> err(res) //KResult<Int,ParseError> -(as)-> <String,ParseError>
     }
 
 fun run(): KResult<String, String> {
-    val res = handle().check { return it.mapErr(ParseError::toString) }
+    val res = handle().check { return it.mapErr(ParseError::toString) } // rust `?` op
     return Ok("yeah!")
 }
 
